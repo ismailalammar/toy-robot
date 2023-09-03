ea2ddd0510a2e53a907166d4c9f611ac763a7917
Ismail Alammar <ismailammr95@gmail.com>
2023-09-03 08:35:50 +0200

    [RED] stop moving when robot reacheds the edge

--- src/test/kotlin/com/idealo/robot/RobotShould.kt
+++ src/test/kotlin/com/idealo/robot/RobotShould.kt
@@ -43,4 +43,21 @@ class RobotShould {
         robot.move()
         assertEquals("0,1,NORTH" , robot.report())
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4 , 5 , 6]) // Number of times to move up
+    fun stop_moving_up_when_robot_reaches_the_edge(movement: Int){
+        val robot = Robot()
+        repeat(movement) {
+            robot.move()
+        }
+        when (movement % 7) {
+            1 -> assertEquals("0,1,NORTH", robot.report())
+            2 -> assertEquals("0,2,NORTH", robot.report())
+            3 -> assertEquals("0,3,NORTH", robot.report())
+            4 -> assertEquals("0,4,NORTH", robot.report())
+            5 -> assertEquals("0,4,NORTH", robot.report())
+            6 -> assertEquals("0,4,NORTH", robot.report())
+        }
+    }
 }
