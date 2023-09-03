bb4f8ff129d633d4cdf49df19cc2d1348223ac5c
Ismail Alammar <ismailammr95@gmail.com>
2023-09-03 06:40:33 +0200

    initial commit

---
+++ src/main/kotlin/com/idealo/robot/RobotApplication.kt
@@ -0,0 +1,11 @@
+package com.idealo.robot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RobotApplication
+
+fun main(args: Array<String>) {
+	runApplication<RobotApplication>(*args)
+}
