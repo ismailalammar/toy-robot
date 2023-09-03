db36cd0e905600642e7f809cef2308aace4b5de7
Ismail Alammar <ismailammr95@gmail.com>
2023-09-03 17:39:35 +0200

    add RobotContoller

---
+++ src/main/kotlin/com/idealo/robot/controller/RobotController.kt
@@ -0,0 +1,45 @@
+package com.idealo.robot.controller
+
+import com.idealo.robot.domain.Direction
+import com.idealo.robot.model.Robot
+import com.idealo.robot.model.RobotPlace
+import com.idealo.robot.service.RobotService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/robot")
+class RobotController(
+    private var robotService: RobotService
+) {
+    private val robot = Robot()
+
+    @PostMapping("/place")
+    fun place(@RequestBody robotPlace: RobotPlace) {
+        robotService.place(robot, robotPlace.x, robotPlace.y, robotPlace.facing)
+    }
+
+    @PostMapping("/left")
+    fun left() {
+        robotService.left(robot)
+    }
+
+    @PostMapping("/right")
+    fun right() {
+        robotService.right(robot)
+    }
+
+
+    @PostMapping("/move")
+    fun move() {
+        robotService.move(robot)
+    }
+
+
+    @PostMapping("/report")
+    fun report() : String {
+        return robotService.report(robot)
+    }
+}
---
+++ src/main/kotlin/com/idealo/robot/model/RobotPlace.kt
@@ -0,0 +1,5 @@
+package com.idealo.robot.model
+
+import com.idealo.robot.domain.Direction
+
+data class RobotPlace(var x:Int , val y: Int , val facing: Direction)
