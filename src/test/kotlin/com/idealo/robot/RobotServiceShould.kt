d15fff219e9632eb78e21a3a28388139948f4cc1
Ismail Alammar <ismailammr95@gmail.com>
2023-09-05 09:32:49 +0200

    [REFACTOR] rename RobotShould to RobotServiceShould

--- src/test/kotlin/com/idealo/robot/RobotServiceShould.kt
+++ src/test/kotlin/com/idealo/robot/RobotServiceShould.kt
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class RobotShould {
+class RobotServiceShould {
     @Autowired private lateinit var robotService: RobotService
     @Autowired private lateinit var robot: Robot
 
